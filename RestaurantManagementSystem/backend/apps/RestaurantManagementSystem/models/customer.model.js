279119c8ca776ec943325cffaa295367072ddf23
PRASHANT CHOUBEY <57279766+prashant-choubey@users.noreply.github.com>
2021-03-09 17:26:19 +0530

    Update customer.model.js
    added module

--- RestaurantManagementSystem/backend/apps/RestaurantManagementSystem/models/customer.model.js
+++ RestaurantManagementSystem/backend/apps/RestaurantManagementSystem/models/customer.model.js
@@ -16,3 +16,29 @@ var customerSchema = mongoose.Schema({
 });
 
 mongoose.model('Customer', customerSchema);
+module.exports =Customer;
+Customer.find((err,docs)=>{
+    if(!err) {
+        console.log(docs);
+        
+    }
+    else 
+        console.log("error");
+})
+
+const cus=new Customer();
+cus.username="Prashant Choubey";
+cus.password="helloworld";
+cus.mobile="8130269652";
+cus.email="prashantchoubey174@gmail.com";
+emp.save((err,doc)=>{
+    if(!err){
+      console.log(doc);
+      
+     
+    }
+    else{
+        console.log("error during adding record"+err.message);
+      
+    }
+});
